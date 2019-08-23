7923c68c615341175b048075ca3b0d87e973be9d
Jose Alberto <jooseplugo@gmail.com>
2019-08-22 23:41:30 -0500

    Add react Apollo and HoC ListOfPhotosCards

--- src/components/ListOfPhotosCards/index.js
+++ src/components/ListOfPhotosCards/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotosCards = () => {
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+// Apollo Client, now with React Hooks https://blog.apollographql.com/apollo-client-now-with-react-hooks-676d116eeae2
+
+const withPhotos = graphql(gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`)
+
+/*
+  { data: { photos = [] } } = {}
+  extrae photos de data
+*/
+const ListOfPhotosCardsComponent = ({ data: { photos = [] } } = {}) => {
+  console.log(photos)
   return (
     <ul>
       {
-        [1, 2, 3, 4].map(id => <PhotoCard key={id} id={id} />)
+        photos.map(photo => <PhotoCard key={photo.id} {...photo} />)
       }
     </ul>
   )
 }
+
+export const ListOfPhotosCards = withPhotos(ListOfPhotosCardsComponent)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
 import { App } from './App'
 
-ReactDom.render(<App />,
+const client = new ApolloClient({
+  uri: 'https://petgram-api-beto.now.sh/graphql'
+})
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app'))
