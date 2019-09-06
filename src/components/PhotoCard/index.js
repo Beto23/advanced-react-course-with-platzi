b93d1d78a6cc518ee9e5968b62d9b854c4140409
Jose Alberto <josee-45@hotmail.com>
2019-09-05 21:39:26 -0500

    Add router detail

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react'
+import { Link } from '@reach/router'
+
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavBotton } from '../FavBotton'
@@ -19,11 +21,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     <Article ref={element}>
       {
         show && <Fragment>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
           <ToggleLikeMutation>
             {
               (toggleLike) => {
